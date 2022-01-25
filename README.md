d3c59e6494b2ab8d4decba55a15c07daf25756a6
hekaiyou <hekaiyou@qq.com>
2022-01-25 17:14:13 +0800

    提交项目

---
+++ README.md
@@ -0,0 +1,8 @@
+# YOLO to MongoDB
+
+## 本地开发
+
+```bash
+python3 -m venv env
+pip install -r requirements.txt
+```
--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from flask import Flask
+import re
+import pymongo
+from datetime import datetime
+from prompt_toolkit import prompt
+from pygments import lex
+from pygments.lexers.jslt import JSLTLexer
+from prompt_toolkit.validation import Validator
+from prompt_toolkit import print_formatted_text
 
-app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def mongo_connection_validator(text):
+    re_text = re.search(
+        r'((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})(\.((2(5[0-5]|[0-4]\d))|[0-1]?\d{1,2})){3}:\d{5}',
+        text, flags=0,
+    )
+    if not re_text:
+        return False
+    client = pymongo.MongoClient(f'mongodb://{re_text.group()}/')
+    db = client['yolo_to_mongo']
+    collection = db['test']
+    collection.insert({'title': 'python connect mongo',
+                       "content": "beautiful", "date": datetime.now()})
+    for obj in collection.find():
+        obj_json = lex(obj, lexer=JSLTLexer())
+        print_formatted_text('Find Test:', obj_json)
+    # 第一个参数找到要更新的文档, 后面是要更新的数据
+    # collection.update({name: "xxx"}, {name: "XXX", args: ...})
+    # 删除集合中的全部数据, 但是集合依然存在, 索引也在
+    collection.remove()
+    # 删除集合 collection
+    # collection.drop()
+    return True
+
+
+def main():
+    mongo_connection = Validator.from_callable(
+        mongo_connection_validator,
+        error_message='This input contains non-numeric characters',
+        move_cursor_to_end=True,
+    )
+    text = prompt('> ', validator=mongo_connection)
+    print('You entered:', text)
+
+
+if __name__ == '__main__':
+    main()
